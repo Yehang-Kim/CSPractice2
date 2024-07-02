828d2da31cc62d4b9c9a7a4dc8fa2dd28f343138
Yehang-Kim <saasaa2y@gmail.com>
2024-07-03 08:53:57 +0900

    #6 23-7. 새도잉

--- CSPractice2/Program.cs
+++ CSPractice2/Program.cs
@@ -8,8 +8,17 @@ namespace CSPractice2
 {
     internal class Program
     {
+        int number = 10;
+
         static void Main(string[] args)
         {
+            // #6 23-8. 상속의 생성자 [건너뛰기]
+            // 은탄환은 없다 = 만병통치약은 없다
+
+
+            // #6 23-7. 새도잉
+            int number = 20;
+            Console.WriteLine(number); // 20
         }
     }
 }
